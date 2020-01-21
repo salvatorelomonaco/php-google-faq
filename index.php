54ed89082b96e9532cda64f737b2afcacb6aca5c
salvatorelomonaco <lommetto95@gmail.com>
2020-01-21 16:48:21 +0100

    Header

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="public/app.css">
+        <title>FAQ - Privacy & Terms - Google</title>
+    </head>
+    <body>
+        <header>
+            <div class="top-header">
+                <div class="logo">
+                    <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt="Google">
+                </div>
+                <div class="privacy-terms-title">
+                    <h2>Privacy & Terms</h2>
+                </div>
+            </div>
+            <div class="bottom-header">
+                <div class="bottom-header-left">
+                    <ul>
+                        <li>Overview</li>
+                        <li>Privacy Policy</li>
+                        <li>Terms of Service</li>
+                        <li>Technogies</li>
+                        <li>FAQ</li>
+                    </ul>
+                </div>
+                <div class="bottom-header-right">
+                    <span>Google Account</span>
+                </div>
+            </div>
+        </header>
+    </body>
+</html>
